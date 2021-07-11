e9b0ae5e340c456720b17ce310056bffa0f6cca4
hansol <kimhan0421@naver.com>
2021-07-11 21:26:10 +0900

    Feat: Api update

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,10 +22,9 @@ public class Posts {
 
     private String author;
 
-    @Builder
-    public Posts(String title, String content, String author) {
+
+    public void update(String title, String content) {
         this.title = title;
         this.content = content;
-        this.author = author;
     }
 }
