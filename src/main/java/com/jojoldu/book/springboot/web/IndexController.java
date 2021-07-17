b6cf223f31d2243ce8bf58662b32c85ea6a3d216
hansol <kimhan0421@naver.com>
2021-07-18 01:55:32 +0900

    Feat: mustache posts page

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,14 +1,22 @@
 package com.jojoldu.book.springboot.web;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
-public class indexController {
+public class IndexController {
 
     @GetMapping("/")
     public String index(){
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
 }
